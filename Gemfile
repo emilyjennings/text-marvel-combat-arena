dfa10014500e50c946499e75d11df21c84e2c9ef
github username <github email address>
2019-01-26 08:41:46 -0500

    getting closer to making these api params work

--- Gemfile
+++ Gemfile
@@ -24,6 +24,9 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'dotenv'
 gem 'faraday'
+gem 'httparty'
+gem 'pry'
+
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/search', to: 'characters#search'
+  post '/search', to: 'characters#character_search'
+  root 'characters#play'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
